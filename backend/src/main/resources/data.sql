29d62e5a0996eb35dfea68440446d15f1a1e17ba
hrusch <hilke.rusch@wps.de>
2025-03-19 14:28:39 +0100

    tiru, hrusch: fügt fehlende Bilder hinzu

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,11 +3,11 @@ INSERT INTO filme
 VALUES
     ('Star Boars', 125, 'assets/Star_Boars.webp', 12, 'Intergalaktische Wildschweine kaempfen gegen das Imperium!', 'Sci-Fi, Comedy', 'Luke Stywalker', 'George Laxus', 'Deutsch'),
     ('The Glitchtrix', 110, 'assets/the_glitchtrix.png', 16, 'Ein Software-Bug offenbart eine digitale Dystopie.', 'Sci-Fi, Comedy', 'Neo Rebooter', 'Lana & Lily Crashowski', 'Finnisch'),
-    ('Space Farce', 140, 'assets/platzhalter.png', 12, 'Eine Crew voller Idioten versucht, das Universum zu retten.', 'Sci-Fi, Action, Comedy', 'Han Yolo', 'J.J. Abrahaha', 'Deutsch'),
+    ('Space Farce', 140, 'assets/Space_Farce.jpeg', 12, 'Eine Crew voller Idioten versucht, das Universum zu retten.', 'Sci-Fi, Action, Comedy', 'Han Yolo', 'J.J. Abrahaha', 'Deutsch'),
     ('The Termi-neigh-tor', 118, 'assets/The_Termi-neigh-tor.webp', 16, 'Ein Cyborg-Pferd aus der Zukunft sorgt fuer Chaos.', 'Sci-Fi, Comedy', 'Arnie Horsenegger', 'James Cameroon', 'Italienisch'),
-    ('Droid Hard', 95, 'assets/platzhalter.png', 6, 'Ein kleiner Roboter nimmt es mit der KI-Mafia auf.', 'Sci-Fi, Animation', 'Bleep Bloop', 'Pete Drinker', 'Deutsch'),
-    ('Guardians of the Lunacy', 130, 'assets/platzhalter.png', 12, 'Eine chaotische Truppe schuetzt das Universum – irgendwie.', 'Sci-Fi, Comedy', 'Chris Plattfall', 'James Gunner', 'Englisch'),
-    ('Back to the Futura', 116, 'assets/platzhalter.png', 12, 'Ein Zeitreisender landet in einer dystopischen Zukunft.', 'Sci-Fi, Adventure', 'Marty McGigawatts', 'Robert Zoomekis', 'Deutsch'),
+    ('Droid Hard', 95, 'assets/Droid_Hard.jpeg', 6, 'Ein kleiner Roboter nimmt es mit der KI-Mafia auf.', 'Sci-Fi, Animation', 'Bleep Bloop', 'Pete Drinker', 'Deutsch'),
+    ('Guardians of the Lunacy', 130, 'assets/Guardians_of_the_Lunacy.jpeg', 12, 'Eine chaotische Truppe schuetzt das Universum – irgendwie.', 'Sci-Fi, Comedy', 'Chris Plattfall', 'James Gunner', 'Englisch'),
+    ('Back to the Futura', 116, 'assets/Back_to_the_Futura.jpeg', 12, 'Ein Zeitreisender landet in einer dystopischen Zukunft.', 'Sci-Fi, Adventure', 'Marty McGigawatts', 'Robert Zoomekis', 'Deutsch'),
     ('Clown Wars: The Honk Awakens', 105, 'assets/Clown_Wars_The_Honk_Awakens.webp', 12, 'Ein Krieg zwischen ausserirdischen Clowns und der Menschheit.', 'Sci-Fi, Horror, Comedy', 'Penny Wisecrack', 'Tim Burtonisch', 'Englisch');
 
 INSERT INTO saele
