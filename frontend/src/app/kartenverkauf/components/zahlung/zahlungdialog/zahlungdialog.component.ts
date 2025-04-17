5cc2fff3e26622e4a00196ce5647f113185d8c0b
hrusch <hilke.rusch@wps.de>
2025-04-17 12:12:17 +0200

    tiru, hrusch: Fügt Geldbetrag zu Zahlungsdialog hinzu

--- frontend/src/app/kartenverkauf/components/zahlung/zahlungdialog/zahlungdialog.component.ts
+++ frontend/src/app/kartenverkauf/components/zahlung/zahlungdialog/zahlungdialog.component.ts
@@ -1,11 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Angebot, Vorstellung} from '../../../dtos/kartenverkauf';
 import {DatePipe} from '@angular/common';
+import {GeldbetragPipe} from '../../../services/geldbetrag.pipe';
 
 @Component({
   selector: 'app-zahlungdialog',
   imports: [
-    DatePipe
+    DatePipe,
+    GeldbetragPipe
   ],
   templateUrl: './zahlungdialog.component.html',
   styleUrl: './zahlungdialog.component.css'
