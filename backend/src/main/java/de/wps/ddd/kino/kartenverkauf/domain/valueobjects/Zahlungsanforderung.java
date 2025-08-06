c16b90b9cfec0c130fb77340f8e0aa2dce84eb7d
Lukas Zühl <lukas.zuehl@wps.de>
2025-08-06 16:50:04 +0200

    lz: Löscht alte Dateien

--- backend/src/main/java/de/wps/ddd/kino/kartenverkauf/domain/valueobjects/Zahlungsanforderung.java
+++
@@ -1,9 +0,0 @@
-package de.wps.ddd.kino.kartenverkauf.domain.valueobjects;
-
-import de.wps.ddd.kino.kartenverkauf.domain.entities.Vorstellung;
-
-public record Zahlungsanforderung(
-        Vorstellung vorstellung,
-        ZusammenhaengendePlaetze plaetze,
-        Geldbetrag betrag) {
-}
