38389fde71b452076ccc100a67640e863879c920
Lukas Zühl <lukas.zuehl@wps.de>
2025-08-14 23:04:00 +0200

    lz: Fügt ArchUnit Test für Paketzyklen hinzu.

--- backend/src/test/java/de/wps/ddd/kino/architecture/ArchitectureTest.java
+++ backend/src/test/java/de/wps/ddd/kino/architecture/ArchitectureTest.java
@@ -4,17 +4,20 @@ import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 import org.jmolecules.archunit.JMoleculesArchitectureRules;
 import org.jmolecules.archunit.JMoleculesDddRules;
 
+@SuppressWarnings("unused")
 @AnalyzeClasses(packages = "de.wps.ddd.kino", importOptions = {ImportOption.DoNotIncludeTests.class})
 public class ArchitectureTest {
 
     @ArchTest
-    @SuppressWarnings("unused")
     ArchRule dddRules = JMoleculesDddRules.all();
 
     @ArchTest
-    @SuppressWarnings("unused")
     ArchRule hexagonalRules = JMoleculesArchitectureRules.ensureHexagonal();
+
+    @ArchTest
+    ArchRule noPackageCycles = SlicesRuleDefinition.slices().matching("(**)").should().beFreeOfCycles();
 }
