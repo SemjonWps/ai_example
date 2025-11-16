58c80fad48946e2ee08c761cf7e1517cf70b7293
Semjon Moessinger <semjon.moessinger@wps.de>
2025-11-16 17:36:33 +0100

    smo: refactoring in kartenverkauf (Part 1)

--- backend/src/main/java/de/wps/ddd/kino/kartenverkauf/adapters/primary/event/mappers/FilmHinzugefuegtEventMapper.java
+++ backend/src/main/java/de/wps/ddd/kino/kartenverkauf/adapters/primary/event/mappers/FilmHinzugefuegtEventMapper.java
@@ -2,18 +2,16 @@ package de.wps.ddd.kino.kartenverkauf.adapters.primary.event.mappers;
 
 import de.wps.ddd.kino.filmauswahl.events.*;
 import de.wps.ddd.kino.kartenverkauf.adapters.secondary.persistence.model.*;
-import java.time.*;
 
 public class FilmHinzugefuegtEventMapper {
 
-    public static VorstellungEntity map(FilmHinzugefuegtEvent event, LocalDateTime beginn, String saal) {
-        var vorstellung = new VorstellungEntity(
-            event.getUuid(),
-                beginn,
-                saal,
-            event.getTitel(),
-            event.getPreis()
+    public static VorstellungEntity map(FilmHinzugefuegtEvent event) {
+        return new VorstellungEntity(
+                event.getUuid(),
+                event.getBeginn(),
+                event.getTitel(),
+                event.getTitel(),
+                event.getPreis()
         );
-        return vorstellung;
     }
 }
--- backend/src/main/java/de/wps/ddd/kino/kartenverkauf/application/fixtures/KartenverkaufFixture.java
+++ backend/src/main/java/de/wps/ddd/kino/kartenverkauf/application/fixtures/KartenverkaufFixture.java
@@ -77,43 +77,11 @@ public class KartenverkaufFixture implements Fixture {
     private void generiereVorstellungenFuerNaechsteFuenfTage(FilmHinzugefuegtEvent event) {
         log.info("Generiere Vorstellungen für Film '{}' für die nächsten 5 Tage", event.getTitel());
 
-        var random = new Random(42);
-        var saele = new String[]{ "großer Saal", "kleiner Saal" };
-
-        // Zeitraum: 14:00 bis 22:00 (2pm bis 10pm)
-        var startStunde = 14;
-        var endStunde = 22;
-        var moeglicheMinuten = new int[]{ 0, 15, 30, 45 };
-
-        var heute = LocalDate.of(2025, 3, 19);
-        var generierteVorstellungen = new ArrayList<VorstellungEntity>();
-
-        // Generiere für die nächsten 5 Tage
-        for (int tag = 0; tag < 5; tag++) {
-            var datum = heute.plusDays(tag);
-
-            // Generiere 1-2 Vorstellungen pro Tag
-            var anzahlVorstellungenProTag = 1 + random.nextInt(2); // 1 oder 2
-
-            for (int i = 0; i < anzahlVorstellungenProTag; i++) {
-                // Zufällige Stunde zwischen 14 und 21 (inclusive)
-                var stunde = startStunde + random.nextInt(endStunde - startStunde);
-                // Zufällige Minuten: 0, 15, 30 oder 45
-                var minute = moeglicheMinuten[random.nextInt(moeglicheMinuten.length)];
-                var zeitslot = LocalTime.of(stunde, minute);
-
-                var beginn = LocalDateTime.of(datum, zeitslot);
-                var saal = saele[random.nextInt(saele.length)];
-
-                var vorstellung = FilmHinzugefuegtEventMapper.map(event, beginn, saal);
-
-                generierteVorstellungen.add(vorstellung);
-            }
-        }
+        var vorstellung = FilmHinzugefuegtEventMapper.map(event);
 
         // Speichere alle generierten Vorstellungen
-        vorstellungRepository.saveAll(generierteVorstellungen);
-        log.info("Vorstellungen generiert und gespeichert: {}", generierteVorstellungen.size());
+        vorstellungRepository.save(vorstellung);
+        log.info("Vorstellung gespeichert: {}");
 
         // Initialisiere Saalpläne für alle generierten Vorstellungen
         var alleVorstellungen = aktuelleVorstellungen.alleVorstellungen();
