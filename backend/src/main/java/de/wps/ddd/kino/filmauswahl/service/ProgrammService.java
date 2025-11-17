eee9fcdaf93861d15653f9adf32a88c71d248089
Semjon Moessinger <semjon.moessinger@wps.de>
2025-11-17 23:37:13 +0100

    smo: refactoring

--- backend/src/main/java/de/wps/ddd/kino/filmauswahl/service/ProgrammService.java
+++ backend/src/main/java/de/wps/ddd/kino/filmauswahl/service/ProgrammService.java
@@ -30,6 +30,7 @@ import java.util.UUID;
 @Slf4j
 public class ProgrammService {
 
+    public static final int MAX_ANZAHL_VORSTELLUNGEN_PRO_TAG = 2;
     private final AktuelleFilme aktuelleFilme;
     private final FilmRepository filmRepository;
     private final FilmauswahlSaalRepository saalRepository;
@@ -63,9 +64,27 @@ public class ProgrammService {
     public void generiereVorstellungenFuerFilm(Film film, List<Saal> saele) {
         log.info("Generiere Vorstellungen für Film: {}", film.getTitel());
 
-        final var random = new Random(42); // Fester Seed für Reproduzierbarkeit
+        final var generierteVorstellungen = generiereVorstellungen(film, saele);
 
-        // Zeitraum: 14:00 bis 22:00 (2pm bis 10pm)
+        vorstellungRepository.saveAll(generierteVorstellungen);
+        log.info("Vorstellungen für Film '{}' generiert: {}", film.getTitel(), generierteVorstellungen.size());
+
+        // Publish domain events
+        for (var vorstellung : generierteVorstellungen) {
+            var event = new FilmHinzugefuegtEvent(
+                    vorstellung.getUuid(),
+                    film.getTitel(),
+                    vorstellung.getPreis(),
+                    vorstellung.getSaal().getName(),
+                    vorstellung.getBeginn());
+
+            domainEventPublisher.publish(event);
+        }
+    }
+
+    private static ArrayList<Vorstellung> generiereVorstellungen(Film film, List<Saal> saele) {
+        final var random = new Random();
+
         final var startStunde = 14;
         final var endStunde = 22;
         final var moeglicheMinuten = new int[]{ 0, 15, 30, 45 };
@@ -73,12 +92,10 @@ public class ProgrammService {
         final var heute = LocalDate.of(2025, 3, 19);
         final var generierteVorstellungen = new ArrayList<Vorstellung>();
 
-        // Generiere für die nächsten 5 Tage
-        for (int tag = 0; tag < 5; tag++) {
+        for (int tag = 0; tag < 4; tag++) {
             var datum = heute.plusDays(tag);
 
-            // Generiere 1-3 Vorstellungen pro Tag
-            var anzahlVorstellungenProTag = 1 + random.nextInt(3);
+            var anzahlVorstellungenProTag = 1 + random.nextInt(MAX_ANZAHL_VORSTELLUNGEN_PRO_TAG);
 
             for (int i = 0; i < anzahlVorstellungenProTag; i++) {
                 // Zufällige Stunde zwischen 14 und 21 (inclusive)
@@ -101,20 +118,6 @@ public class ProgrammService {
                 generierteVorstellungen.add(vorstellung);
             }
         }
-
-        vorstellungRepository.saveAll(generierteVorstellungen);
-        log.info("Vorstellungen für Film '{}' generiert: {}", film.getTitel(), generierteVorstellungen.size());
-
-        // Publish domain events
-        for (var vorstellung : generierteVorstellungen) {
-            var event = new FilmHinzugefuegtEvent(
-                    vorstellung.getUuid(),
-                    film.getTitel(),
-                    vorstellung.getPreis(),
-                    vorstellung.getSaal().getName(),
-                    vorstellung.getBeginn());
-
-            domainEventPublisher.publish(event);
-        }
+        return generierteVorstellungen;
     }
 }
